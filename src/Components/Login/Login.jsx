a9a546f04c5dd608033675ec8afd4da1e3b8b2d4
Amrit Kumar <amritkum360@gmail.com>
2024-01-20 22:52:35 +0530

    Designed the login page using bootstrap

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,47 +1,38 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import './Login.css'
-
+import './Login.css';
 import { URL } from '../Auth/Auth';
-
 import { useAuth } from "../Auth/AuthContext";
-
 
 function Login() {
     const navigate = useNavigate();
     const { setToken } = useAuth();
 
-    // user input states 
     const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
 
-    // user input handlers 
     const handlePhoneNumberChange = (event) => {
         setPhoneNumber(event.target.value);
     };
+
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
     };
 
-    // form submission handler function 
     const loginSubmitHandler = async (event) => {
-
         event.preventDefault();
         const userData = {
             phone_number: phoneNumber,
             password: password
-        }
+        };
 
-        // Use the Fetch API to make a POST request
         await fetch(URL + '/api/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(userData),
-
         }).then(response => {
-
             response.json().then(data => {
                 if (data.token) {
                     localStorage.setItem("token", data.token);
@@ -50,57 +41,50 @@ function Login() {
                 navigate('/dashboard');
             })
         }).catch(error => {
-            // Handle any errors that occurred during the fetch
-            console.error('Error registering user:', error.message);
-            return <p>error.message</p>
+            console.error('Error logging in user:', error.message);
+            return <p>{error.message}</p>
         });
     }
 
     return (
-        <div className="login-form">
-            <form className="form-container" onSubmit={loginSubmitHandler}>
+        <div className="container">
+       <img src="\src\assets\logo-bgremoved.png" alt="logo" style={{width:"80px"}} />
+
+            <form className="form-container mt-2" onSubmit={loginSubmitHandler}>
                 <h1>Login</h1>
 
-                {/* phone number input  */}
-                <div className="form-group row">
-                    <label htmlFor="phone_number" className="col-form-label">phone Number</label>
-                    <div className="col-sm-10">
-                        <input
-                            type="text"
-                            className="form-control login-input"
-                            id="inputEmail3"
-                            placeholder="Enter your phone Number"
-                            onChange={handlePhoneNumberChange}
-                            value={phoneNumber}
-                        />
-                    </div>
+                <div className="mb-3">
+                    <label htmlFor="phone_number" className="form-label">Phone Number</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="inputEmail3"
+                        placeholder="Enter your Phone Number"
+                        onChange={handlePhoneNumberChange}
+                        value={phoneNumber}
+                    />
                 </div>
 
-                {/* password input  */}
-                <div className="form-group row">
-                    <label htmlFor="password" className="col-form-label">Password</label>
-                    <div className="col-sm-10">
-                        <input
-                            type="password"
-                            className="form-control login-input"
-                            id="inputPassword3"
-                            placeholder="Enter your Password"
-                            onChange={handlePasswordChange}
-                            value={password}
-                        />
-                    </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="inputPassword3"
+                        placeholder="Enter your Password"
+                        onChange={handlePasswordChange}
+                        value={password}
+                    />
                 </div>
 
-                {/* submit button  */}
-                <div className="form-group row">
-                    <div className="col-lg-10">
-                        <button type="submit" className="btn btn-primary signup-btn">LogIn</button>
-                        <Link to="/signup">don't have account?</Link>
-                    </div>
+                <div className="mb-3">
+                    <button type="submit" className="btn btn-warning" 
+                style={{ width: '100%' }}>Log In</button>
+                    <Link to="/signup" className="mt-2 d-block text-center">Don't have an account?</Link>
                 </div>
             </form>
         </div>
     )
 }
 
-export default Login
+export default Login;
