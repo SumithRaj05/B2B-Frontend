c8b7b56d9b87ffbaa47cda515b54a5a0c9cb7e22
Amrit Kumar <amritkum360@gmail.com>
2024-01-27 21:43:47 +0530

    added icons in categorybar dashboard

--- src/Components/Account/Dashboard/components/Dash_category.jsx
+++ src/Components/Account/Dashboard/components/Dash_category.jsx
@@ -3,18 +3,32 @@ import React, { useState, useRef, useEffect } from "react";
 const Dash_category = ({ highlightButton }) => {
   const categories = [
     "all",
-    "agri products & equipments",
-    "apparel and fashion",
-    "architects & interior designing",
-    "automobile parts & spares",
-    "chemicals,dyes & solvents",
-    "construction & real estate",
-    "consumer electronics",
-    "electricals & electronics",
-    "energy and power"
+    "Agri products & Equipments",
+    "Apparel & Fashion",
+    "Automobile Spares & parts",
+    "Bags and Luggage",
+    "Beauty, Cosmetics & Care",
+    "Books, Stationery & Office supplies",
+    "Chemicals, dyes & solvents",
+    "Construction supplies",
+    "Electricals & lights",
+    "Electronics & Computers",
+    "Footwear",
+    "Furniture & Interior Decorative",
+    "Hardware & paints",
+    "Home & kitchen appliances",
+    "Industrial equipments",
+    "Tiles & Ceramics"
   ];
 
   const links = [
+    "#",
+    "#",
+    "#",
+    "#",
+    "#",
+    "#",
+    "#",
     "#",
     "#",
     "#",
@@ -30,17 +44,25 @@ const Dash_category = ({ highlightButton }) => {
   // Add image sources array
   const imageSources = [
     "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
+    "category-img/agri-pump.png",
+    "category-img/t-shirt.png",
+    "category-img/automobile.png",
+    "category-img/luggage.png",
+    "category-img/cosmetics .png",
+    "category-img/stationary.png",
+    "category-img/chemicals.png",
+    "category-img/construction.png",
+    "category-img/electrical.png",
     "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
-    "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
+    "category-img/footware.png",
+    "category-img/furniture.png",
+    "category-img/hardware.png",
+    "category-img/appliances.png",
+    "category-img/equipment.png",
+    "category-img/tiles.png",
   ];
 
+  const [selectedCategory, setSelectedCategory] = useState("all");
   const [scrollX, setScrollX] = useState(0);
   const [scrollEnd, setScrollEnd] = useState(false);
   const scrl = useRef();
@@ -82,10 +104,35 @@ const Dash_category = ({ highlightButton }) => {
     }
   };
 
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+      };
+
+  const handleImageClick = (category) => {
+    setSelectedCategory(category);
+  };
+
   const categoryItems = categories.map((category, index) => (
     <div key={category + index} style={{ marginRight: "20px" }}>
-      <a href={links[index]} style={{ textDecoration: "none", display: "flex", flexDirection: "column" , alignItems:"center" }}>
-        <img src={imageSources[index]} alt="" style={{ height: "auto", width: "80px" }} />
+      <a
+        href={links[index]}
+        style={{
+          textDecoration: "none",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          borderBottom:
+            selectedCategory === category ? "2px solid blue" : "none"
+        }}
+        onClick={() => handleCategoryClick(category)}
+      >
+        <img
+        href={links[index]}
+          src={imageSources[index]}
+          alt=""
+          style={{ height: "80px", width: "auto" }}
+          onClick={() => handleImageClick(category)}
+        />
         <p>{category}</p>
       </a>
     </div>
@@ -95,21 +142,23 @@ const Dash_category = ({ highlightButton }) => {
     <div className="App">
       <div className="cargy-scroll-style">
         <button className="prev s-button" onClick={() => slide(-800)}>
-          <span class="material-symbols-outlined">
-chevron_left
-</span>
+          <span className="material-symbols-outlined">chevron_left</span>
         </button>
-        <ul className="s-ul" ref={scrl} onClick={(e) => highlightButton(e.target)} onScroll={scrollCheck} style={{ overflowX: "scroll", whiteSpace: "nowrap" }}>
+        <ul
+          className="s-ul"
+          ref={scrl}
+          onClick={(e) => highlightButton(e.target)}
+          onScroll={scrollCheck}
+          style={{ overflowX: "scroll", whiteSpace: "nowrap" }}
+        >
           {categoryItems}
         </ul>
         <button className="next s-button" onClick={() => slide(800)}>
-          <span class="material-symbols-outlined">
-chevron_right
-</span>
+          <span className="material-symbols-outlined">chevron_right</span>
         </button>
       </div>
     </div>
   );
 };
 
-export default Dash_category;
+export default Dash_category
--- src/Components/Home/components/Explore.jsx
+++ src/Components/Home/components/Explore.jsx
@@ -12,7 +12,7 @@ export default function Explore(){
         </div>
         <div className="card-group explore-grp">
   <div className="card">
-    <img src="https://img.freepik.com/premium-psd/youtube-video-thumbnail-start-trading-today_475351-168.jpg" className="card-img-top" alt="..." />
+    <img src="/home/factory.jpg" className="card-img-top" alt="..." />
     <div className="card-body">
       <h5 className="card-title">Browse Manufacturers</h5>
       <p className="card-text">Connecting Bejinesses with Trusted manufaturer: Our B2B marketplace facilitates linking buyers with reputable mahufacturer, ensuring products at competitive prices</p>
@@ -20,7 +20,7 @@ export default function Explore(){
     </div>
   </div>
   <div className="card">
-    <img src="https://img.freepik.com/premium-psd/youtube-video-thumbnail-start-trading-today_475351-168.jpg" className="card-img-top" alt="..." />
+    <img src="/home/wholesaler.jpg" className="card-img-top" alt="..." />
     <div className="card-body">
     <h5 className="card-title">Find Wholesaler Suppliers</h5>
     <p className="card-text">Unloack access to top watch product of competitive rates. Our platform simplifies the search for trusted wholesalers, ensuring quality souring tailored to your requirements</p>
@@ -28,7 +28,7 @@ export default function Explore(){
     </div>
   </div>
   <div className="card">
-    <img src="https://img.freepik.com/premium-psd/youtube-video-thumbnail-start-trading-today_475351-168.jpg" className="card-img-top" alt="..." />
+    <img src="/home/retailer.jpg" className="card-img-top" alt="..." />
     <div className="card-body">
       <h5 className="card-title">Retailer Resellers Discover</h5>
       <p className="card-text">Expand your Reach with Bejiness.com Whether entering new cities, exploring divrse industries, or aiming for expanding Bejiness our platform empowers you to seamlessly reach with the ideal retailers for your Bejiness.</p>
