1afef942e66495c9b92c05d5075fdc65417181de
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2024-02-01 21:00:32 +0530

    bugs fixed

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,3 +1,3 @@
-export const URL = "http://127.0.0.1:5000"
+// export const URL = "http://127.0.0.1:5000"
 
-// export const URL = "https://bejiness-api.onrender.com"
+export const URL = "https://bejiness-api.onrender.com"
